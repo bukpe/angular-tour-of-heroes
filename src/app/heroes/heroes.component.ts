6ab3503abeba50cc5872493d29dbe1619793e75d
bukpe <b.ukpe@libero.it>
2021-10-26 15:10:34 +0200

    nav routing added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,10 +13,7 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
@@ -24,7 +21,6 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
